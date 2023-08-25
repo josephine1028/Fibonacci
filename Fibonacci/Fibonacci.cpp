ea9478dbacb3861ef857f96553063ea7ea1f3cf3
josephine1028 <josephine@ati2000.co.kr>
2023-08-25 10:53:19 +0900

    초기 커밋.

---
+++ Fibonacci/Fibonacci.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Fibo(int n)
+{
+	if (n <= 1)
+		return n;
+	else
+		return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main()
+{
+	int N, res;
+	scanf_s("%d", &N);
+	printf("%d", Fibo(N));
+
+	system("pause");
+	return 0;
+}
